cf59bd87cb9cd85172e9e9444a8c48b95bed8233
Sumit Deo <sumit.deo@outlook.com>
2020-05-12 09:38:54 -0400

    Chapter1-fundamentals.basicProgrammingModel-Ex2

---
+++ src/fundamentals/basicProgrammingModel/Exercise2.java
@@ -0,0 +1,19 @@
+package fundamentals.basicProgrammingModel;
+
+/**
+ * @author Sumit Deo
+ */
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        double solutionA = (1 + 2.236)/2;
+        double solutionB = 1 + 2 + 3 + 4.0;
+        boolean solutionC = 4.1 >= 4;
+        String solutionD = 1 + 2 + "3";
+
+        System.out.println("a) " + solutionA);
+        System.out.println("b) " + solutionB);
+        System.out.println("c) " + solutionC);
+        System.out.println("d) " + solutionD);
+    }
+}
