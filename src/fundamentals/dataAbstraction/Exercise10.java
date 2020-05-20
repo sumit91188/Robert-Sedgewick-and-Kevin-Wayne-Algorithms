c54ebc1225cdad28bfdaba6b9fd7396ca0329806
Sumit Deo <sumit.deo@outlook.com>
2020-05-20 14:47:42 -0400

    Chapter1-fundamentals.dataAbstraction-Ex4-10 - updated

--- src/fundamentals/dataAbstraction/Exercise10.java
+++ src/fundamentals/dataAbstraction/Exercise10.java
@@ -20,45 +20,45 @@ public class Exercise10 {
         visualCounter.decrement();
         visualCounter.decrement();
     }
+}
 
-    public static class VisualCounter {
+class VisualCounter {
 
-        private int n;
-        private int max;
-        int operationsTally;
-        int countTally;
+    private int n;
+    private int max;
+    int operationsTally;
+    int countTally;
 
-        public VisualCounter(int n, int max) {
-            this.n = n;
-            this.max = max;
+    public VisualCounter(int n, int max) {
+        this.n = n;
+        this.max = max;
 
-            StdDraw.setCanvasSize(1024, 512);
-            StdDraw.setPenRadius(.015);
-            StdDraw.setXscale(0, n+1);
-            StdDraw.setYscale(-max-3, max+3);
+        StdDraw.setCanvasSize(1024, 512);
+        StdDraw.setPenRadius(.015);
+        StdDraw.setXscale(0, n+1);
+        StdDraw.setYscale(-max-3, max+3);
+    }
+
+    void increment() {
+
+        if (operationsTally <= n && countTally <= max) {
+            operationsTally++;
+            countTally++;
         }
+        plotChart();
+    }
 
-        private void increment() {
+    void decrement() {
 
-            if (operationsTally <= n && countTally <= max) {
-                operationsTally++;
-                countTally++;
-            }
-            plotChart();
+        if (operationsTally <= n) {
+            operationsTally++;
+            countTally--;
         }
+        plotChart();
+    }
 
-        private void decrement() {
+    private void plotChart() {
 
-            if (operationsTally <= n) {
-                operationsTally++;
-                countTally--;
-            }
-            plotChart();
-        }
-
-        private void plotChart() {
-
-            StdDraw.point(operationsTally, countTally);
-        }
+        StdDraw.point(operationsTally, countTally);
     }
 }
